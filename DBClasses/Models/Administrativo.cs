846cdca8dfdb9dd6bb3ef4ff49948ce945155b5b
Carlos F <103224983+F3nr1@users.noreply.github.com>
2022-08-24 23:06:56 -0500

    Add files via upload

---
+++ DBClasses/Models/Administrativo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBTallerM.Models
+{
+    public partial class Administrativo
+    {
+        public int AdmId { get; set; }
+        public string? NivelEstudio { get; set; }
+        public int PersonaId { get; set; }
+    }
+}
