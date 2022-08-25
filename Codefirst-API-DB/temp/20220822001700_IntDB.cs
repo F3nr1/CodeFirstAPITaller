b4e62904563108d4f508efc10f6f9e5d3ce1aafe
Carlos F <103224983+F3nr1@users.noreply.github.com>
2022-08-24 23:08:06 -0500

    Add files via upload

---
+++ Codefirst-API-DB/temp/20220822001700_IntDB.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBTallerM.Migrations
+{
+    public partial class IntDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Direccion",
+                columns: table => new
+                {
+                    DireccionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Zona = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoCalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Num1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Num2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Num3 = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direccion", x => x.DireccionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    PersonaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<int>(type: "int", nullable: false),
+                    DireccionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.PersonaID);
+                    table.ForeignKey(
+                        name: "FK_Persona_Direccion_DireccionID",
+                        column: x => x.DireccionID,
+                        principalTable: "Direccion",
+                        principalColumn: "DireccionID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Administrativo",
+                columns: table => new
+                {
+                    AdminID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NivelEstudio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administrativo", x => x.AdminID);
+                    table.ForeignKey(
+                        name: "FK_Administrativo_Persona_PersonaID",
+                        column: x => x.PersonaID,
+                        principalTable: "Persona",
+                        principalColumn: "PersonaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteID);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Persona_PersonaID",
+                        column: x => x.PersonaID,
+                        principalTable: "Persona",
+                        principalColumn: "PersonaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mecanico",
+                columns: table => new
+                {
+                    MecanicoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NivelEstudio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mecanico", x => x.MecanicoID);
+                    table.ForeignKey(
+                        name: "FK_Mecanico_Persona_PersonaID",
+                        column: x => x.PersonaID,
+                        principalTable: "Persona",
+                        principalColumn: "PersonaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Administrativo_PersonaID",
+                table: "Administrativo",
+                column: "PersonaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_PersonaID",
+                table: "Cliente",
+                column: "PersonaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mecanico_PersonaID",
+                table: "Mecanico",
+                column: "PersonaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persona_DireccionID",
+                table: "Persona",
+                column: "DireccionID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Administrativo");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Mecanico");
+
+            migrationBuilder.DropTable(
+                name: "Persona");
+
+            migrationBuilder.DropTable(
+                name: "Direccion");
+        }
+    }
+}
